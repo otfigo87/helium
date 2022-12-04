5ff0fddc94301daa513ad3c7d6e4190dec2d5191
Otmane Aatik <aaotmane@gmail.com>
2022-12-04 13:56:15 -0500

    mouse event

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+const mouses = document.querySelectorAll(".mouse");
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+window.addEventListener("mousemove", (e) => {
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.y + "px";
+        mouse.style.left = e.x + "px";
+    })
+})
 
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
